df9cbdbe6795f6715f92f40891ad2c304a4c59e9
onumaeleanyagift <onumaeleanyagift@gmail.com>
2025-03-14 15:44:17 +0100

    imported COunter(jsx)

--- alx-react-app-new/src/App.jsx
+++ alx-react-app-new/src/App.jsx
@@ -2,6 +2,12 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
+import WelcomeMessage from './components/WelcomeMessage'
+import Header from './components/Header'
+import MainContent from './components/MainContent'
+import Footer from './components/Footer'
+import UserProfile from "./components/UserProfile"
+import Counter from "./components/Counter"
 
 function App() {
   const [count, setCount] = useState(0)
@@ -28,8 +34,15 @@ function App() {
       <p className="read-the-docs">
         Click on the Vite and React logos to learn more
       </p>
+      <WelcomeMessage />
+      <Header />
+      <Counter />
+      <MainContent />
+      <UserProfile
+      name="Alice" age="25" bio="Loves hiking and photography" />
+      <Footer />
     </>
-  )
+  );
 }
 
 export default App
