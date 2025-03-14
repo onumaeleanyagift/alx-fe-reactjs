da35d20621574cbb38e8f24dd57e0336242e9fd9
onumaeleanyagift <onumaeleanyagift@gmail.com>
2025-03-14 16:41:38 +0100

    modified & added function

--- alx-react-app-props/src/components/UserProfile.jsx
+++ alx-react-app-props/src/components/UserProfile.jsx
@@ -1,17 +1,7 @@
+import React, { useContext } from "react";
+
 function UserProfile() {
-  const UserProfile = (props) => {
-    return (
-      <div
-        style={{ border: "1px solid gray", padding: "10px", margin: "10px" }}
-      >
-        <h2 style={{ color: "blue" }}>{props.name}</h2>
-        <p>
-          Age: <span style={{ fontWeight: "bold" }}> {props.age}</span>{" "}
-        </p>
-        <p>Bio: {props.bio}</p>
-      </div>
-    );
-  };
+  return;
 }
 
 export default UserProfile;
