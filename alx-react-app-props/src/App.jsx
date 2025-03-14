c9f2bd3c808f6dc2cd2297546081229f82e79fb7
onumaeleanyagift <onumaeleanyagift@gmail.com>
2025-03-14 16:37:05 +0100

    import User profile

--- alx-react-app-props/src/App.jsx
+++ alx-react-app-props/src/App.jsx
@@ -4,6 +4,7 @@ import viteLogo from "/vite.svg";
 import "./App.css";
 import ProfilePage from "./components/ProfilePage";
 import UserContext from "./components/UserContext";
+import UserProfile from "./src/components/UserProfile";
 
 function App() {
   const userData = { name: "Jane Doe", email: "jane.doe@example.com" };
@@ -12,6 +13,8 @@ function App() {
   <UserContext.Provider value={userData}>
     <ProfilePage userData={userData} />
   </UserContext.Provider>;
+
+   <UserProfile name="Alice" age="25" bio="Loves hiking and photography" />;
 }
 
 export default App;
