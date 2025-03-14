e2f3cbf98700511a27d2c008b3eb1b3cef737430
onumaeleanyagift <onumaeleanyagift@gmail.com>
2025-03-14 14:41:00 +0100

    Main Content

--- alx-react-app-new/src/components/MainContent.jsx
+++ alx-react-app-new/src/components/MainContent.jsx
@@ -1,6 +1,6 @@
 function MainContent() {
     return (
-      <main>
+      <main style={{ backgroundColor: 'gray', color: 'navy', margin: '0'}}>
         <p>I love to visit New York, Paris, and Tokyo.</p>
       </main>
     );
