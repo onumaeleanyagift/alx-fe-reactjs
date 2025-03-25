2a790a57f6500de6805cb18cabe1b98657ff3e11
onumaeleanyagift <onumaeleanyagift@gmail.com>
2025-03-25 08:30:10 +0100

    imported the correct path (line 7)

--- recipe-sharing-app/src/components/RecipeList.jsx
+++ recipe-sharing-app/src/components/RecipeList.jsx
@@ -1,19 +1,18 @@
-
-import { useRecipeStore } from './useRecipeStore';
+import { useRecipeStore } from "./recipeStore";
 
 const RecipeList = () => {
-    const recipes = useRecipeStore(state => state.recipes);
+  const recipes = useRecipeStore((state) => state.recipes);
 
-    return (
-        <div>
-            {useRecipeStore.map((recipe) => (
-                <div key={recipe.iid}>
-                    <h3>{recipe.title}</h3>
-                    <p>{recipe.descriptin}</p>
-                </div>
-            ))}
+  return (
+    <div>
+      {useRecipeStore.map((recipe) => (
+        <div key={recipe.iid}>
+          <h3>{recipe.title}</h3>
+          <p>{recipe.descriptin}</p>
         </div>
-    );
+      ))}
+    </div>
+  );
 };
 
 export default RecipeList;
