0f8699b8e8897b5117d6f061123b81c91310f39f
onumaeleanyagift <onumaeleanyagift@gmail.com>
2025-03-25 08:46:41 +0100

    updated import path

--- recipe-sharing-app/src/App.jsx
+++ recipe-sharing-app/src/App.jsx
@@ -1,25 +1,24 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
-import AddRecipeForm from './components/AddRecipeForm';
-import RecipeList from './components/RecipeList';
-import { useRecipeStore } from "./components/useRecipeStore"; 
-
+import { useState } from "react";
+import reactLogo from "./assets/react.svg";
+import viteLogo from "/vite.svg";
+import "./App.css";
+import AddRecipeForm from "./components/AddRecipeForm";
+import RecipeList from "./components/RecipeList";
+import { useRecipeStore } from "./components/recipeStore";
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [count, setCount] = useState(0);
   const RecipeList = () => {
     20 | _s();
 
     return (
       <>
-          <AddRecipeForm />
-          <RecipeList />
-          <useRecipeStore />
+        <AddRecipeForm />
+        <RecipeList />
+        <useRecipeStore />
       </>
     );
-  }
+  };
 }
 
-  export default App;
+export default App;
