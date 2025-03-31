c3a712ea5115fb1c93fbf2af8fd66a17d69830ee
onumaeleanyagift <onumaeleanyagift@gmail.com>
2025-03-31 22:20:55 +0100

    Extended GitHub API integration: Added support for location and repository count filters in user search functionality

--- github-user-search/src/services/githubService.js
+++ github-user-search/src/services/githubService.js
@@ -1,7 +1,11 @@
 import axios from "axios";
 
-export const fetchUserData = async (query) => {
+export const fetchUserData = async (username, location, minRepos) => {
   try {
+    let query = username;
+    if (location) query += `+location:${location}`;
+    if (minRepos) query += `+repos:>${minRepos}`;
+
     const response = await axios.get(
       `https://api.github.com/search/users?q=${query}`,
       {
