8f65f6b90a5eff3071bb9ac9a5f4176337ee5054
onumaeleanyagift <onumaeleanyagift@gmail.com>
2025-03-31 22:03:54 +0100

    amend

--- github-user-search/src/components/Search.jsx
+++ github-user-search/src/components/Search.jsx
@@ -34,7 +34,7 @@ function Search() {
         <button type="submit">Search</button>
       </form>
       {loading && <p>Loading...</p>}
-      {error && <p>Looks like we can't find the user.</p>}
+      {error && <p>Looks like we cant find the user.</p>}
       {userData && (
         <div>
           <img src={userData.avatar_url} alt={userData.login} width="100" />
